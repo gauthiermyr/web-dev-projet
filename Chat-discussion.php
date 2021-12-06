28f97cc1979af149fd8a78c1417afbd153ea1fe5
Souhail <souhail.aitlahcen@gmail.com>
2021-12-06 17:09:34 +0100

    Ajout page profil utilisateur et page chat (celle-ci à améliorer (moi)))

---
+++ Chat-discussion.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./public/css/template.css" rel="stylesheet">
+    <title>LeankDingue</title>
+</head>
+<body>
+    <div class="navigation">
+        <div id="nav-container">
+            <div class="nav-element">
+                <img src="./public/img/LeankDingue.png"></img>
+            </div>
+            <div class="nav-element">
+                <input id="input-nav-bar" type="text" placeholder="Recherche..."></input>
+                <button class="hover-translate" onclick="search()">Rechercher</button>
+            </div>
+            <div class="nav-element text-white">
+                <span onclick="alert('login')" class="clickable hover-translate">Login</span>
+                <span onclick="alert('register')" class="clickable hover-translate">Register</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-Chat">
+        <div id="nav-connaissance">Rechercher connaissance </div>
+        <div id="nav-dicussion">Discussion </div>
+        <div id="nav-container-Chat"> </div>
+        <div id="nav-chat-mot">Chat </div>
+        <div id="nav-block-contacts1"></div>
+        <div id="nav-Nom-Prenom1"> Prénom Nom </div>
+        <div id="nav-ligne-block1"> Première ligne du dernier message envoyé ... </div>
+        <div id="nav-trait-separateur1"> </div>
+        <div id="nav-block-contacts2"></div>
+        <div id="nav-Nom-Prenom2"> Prénom Nom </div>
+        <div id="nav-ligne-block2"> Première ligne du dernier message envoyé ... </div>
+        <div id="nav-trait-separateur2"> </div>
+        <div id="nav-block-contacts3"></div>
+        <div id="nav-Nom-Prenom3"> Prénom Nom </div>
+        <div id="nav-ligne-block3"> Première ligne du dernier message envoyé ... </div>
+        <div id="nav-trait-separateur3"> </div>
+        <div id="nav-block-contacts4"></div>
+        <div id="nav-Nom-Prenom4"> Prénom Nom </div>
+        <div id="nav-ligne-block4"> Première ligne du dernier message envoyé ... </div>
+        <div id="nav-trait-separateur4"> </div>
+
+        <div class="post-navigation-profile">
+            <img src="./public/img/profil.png"></img>
+                <div class="post-navigation-profile-info">
+                    <h4>Alpha 7</h4>
+                    <h5>Full Stack</h5>
+                </div>
+        </div>
+
+        <div id="nav-block-message"></div>
+        <div id="nav-message-recu"> Salut comment vas-tu ? </div>
+
+        <div id="nav-votre-message"> Votre Message : </div>
+        <div class="nav-element">
+                <input id="input-nav-bar" type="text" placeholder="Message..."></input>
+                <button class="hover-translate" onclick="search()">Envoyer</button>
+        </div>
+
+
+        <div id="nav-Bas-de-Page">  </div>
+
+    </div>
+
+    <script src="./public/js/main.js"></script>
+</body>
+</html>
