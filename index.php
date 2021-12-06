7f1b294aa8ffe037fb98d420b2788cc70ca31eaf
Pibastte <baptiste@feachy.com>
2021-12-06 13:40:00 +0100

    added container

--- index.php
+++ index.php
@@ -15,13 +15,39 @@
             </div>
             <div class="nav-element">
                 <input id="input-nav-bar" type="text" placeholder="Recherche..."></input>
-                <button>Rechercher</button>
+                <button class="hover-translate" onclick="search()">Rechercher</button>
             </div>
             <div class="nav-element text-white">
-                <span>Login</span>
-                <span>Register</span>
+                <span onclick="alert('login')" class="clickable hover-translate">Login</span>
+                <span onclick="alert('register')" class="clickable hover-translate">Register</span>
             </div>
         </div>
     </div>
+
+    <div class="container">
+        <div id="post-creation-container">
+            Nouveau post
+        </div>
+        <div id="post-feed">
+            <div class="post-container">
+                <div class="post-navigation">
+                    <div>01/12/2021 14h16</div>
+                    <div>
+                        <img src="./public/img/profil.png"></img>
+                        <h4>Alpha 7</h4>
+                        <h5>Full Stack</h5>
+                    </div>
+                </div>
+                <div class="post-body">
+                    <p>Fichier data csv !</p>
+                </div>
+                <div class="post-footer">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="./public/js/main.js"></script>
 </body>
 </html>
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,14 @@
 // Global script
+
+function search() {
+    if (document.querySelector("#input-nav-bar")?.value.trim() != "") {
+        alert("searching for " + document.querySelector("#input-nav-bar")?.value + "...");
+    }
+    else {
+        alert("please enter a search query");
+    }
+}
+
+(() => {
+    //conde to execute on load
+})();
