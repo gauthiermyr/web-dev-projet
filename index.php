e21fc2e6a47acf3fe57e01b194b28c11bdfcb580
Pibastte <baptiste@feachy.com>
2021-12-06 12:47:25 +0100

    Created navigation

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
-<?php
-
-// Affiche toutes les informations, comme le ferait INFO_ALL
-phpinfo();
-
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./public/css/template.css" rel="stylesheet">
+    <title>LeankDingue</title>
+</head>
+<body>
+    <div class="navigation">
+        <div id="nav-container">
+            <div class="nav-element">
+                <img src="./public/img/LeankDingue.png"></img>
+            </div>
+            <div class="nav-element">
+                <input id="input-nav-bar" type="text" placeholder="Recherche..."></input>
+                <button>Rechercher</button>
+            </div>
+            <div class="nav-element text-white">
+                <span>Login</span>
+                <span>Register</span>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
