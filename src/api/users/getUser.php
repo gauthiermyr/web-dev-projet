f4ef02dc544ff062eb17cde0baa41637c6ee6f78
Gauthier Mayer <legrangm@gmail.com>
2021-12-06 15:30:21 +0100

    Login and logout api
    Use session

--- src/api/users/getUser.php
+++ src/api/users/getUser.php
@@ -1,13 +1,17 @@
 <?php
 
 require('../../db/database.php');
+//
+//if (!isset($_GET['id'])){
+//    echo json_encode(["success" => false]);
+//}
+else{
+    $stmt = $db->query("SELECT * FROM utilisateurs");
+    $users = [];
+    while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+        array_push($users, $row);
+    }
 
-$stmt = $db->query("SELECT * FROM utilisateurs");
-$users = [];
-while ($row = $stmt->fetch()) {
-    array_push($users, $row);
+    echo json_encode($users);
 }
-
-echo json_encode($users);
-
 
