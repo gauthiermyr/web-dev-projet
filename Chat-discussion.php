8ce89f58d3c27a7610fc438066c3ff4f97aeaf01
Souhail <souhail.aitlahcen@gmail.com>
2021-12-07 13:06:11 +0100

    Ajout page chat (mais manque dans le blockbas conditions ge ect))

--- Chat-discussion.php
+++ Chat-discussion.php
@@ -27,44 +27,75 @@
     <div class="container-Chat">
         <div id="nav-connaissance">Rechercher connaissance </div>
         <div id="nav-dicussion">Discussion </div>
-        <div id="nav-container-Chat"> </div>
-        <div id="nav-chat-mot">Chat </div>
-        <div id="nav-block-contacts1"></div>
-        <div id="nav-Nom-Prenom1"> Prénom Nom </div>
-        <div id="nav-ligne-block1"> Première ligne du dernier message envoyé ... </div>
-        <div id="nav-trait-separateur1"> </div>
-        <div id="nav-block-contacts2"></div>
-        <div id="nav-Nom-Prenom2"> Prénom Nom </div>
-        <div id="nav-ligne-block2"> Première ligne du dernier message envoyé ... </div>
-        <div id="nav-trait-separateur2"> </div>
-        <div id="nav-block-contacts3"></div>
-        <div id="nav-Nom-Prenom3"> Prénom Nom </div>
-        <div id="nav-ligne-block3"> Première ligne du dernier message envoyé ... </div>
-        <div id="nav-trait-separateur3"> </div>
-        <div id="nav-block-contacts4"></div>
-        <div id="nav-Nom-Prenom4"> Prénom Nom </div>
-        <div id="nav-ligne-block4"> Première ligne du dernier message envoyé ... </div>
-        <div id="nav-trait-separateur4"> </div>
-
-        <div class="post-navigation-profile">
-            <img src="./public/img/profil.png"></img>
-                <div class="post-navigation-profile-info">
-                    <h4>Alpha 7</h4>
-                    <h5>Full Stack</h5>
-                </div>
+        <div id="nav-container-Chat-Whole">
+            <div id="nav-container-Chat"> </div>
+            <div id="nav-chat-mot">Chat </div>
+            <div id="nav-block-contacts1">
+                <div id="nav-Nom-Prenom1"> Prénom Nom </div>
+                <div id="nav-ligne-block1"> Première ligne du dernier message envoyé ... </div>
+                <div id="nav-trait-separateur1"> </div>
+            </div>
+            <div id="nav-block-contacts2">
+                <div id="nav-Nom-Prenom2"> Prénom Nom </div>
+                <div id="nav-ligne-block2"> Première ligne du dernier message envoyé ... </div>
+                <div id="nav-trait-separateur2"> </div>
+            </div>
+            <div id="nav-block-contacts3">
+                <div id="nav-Nom-Prenom3"> Prénom Nom </div>
+                <div id="nav-ligne-block3"> Première ligne du dernier message envoyé ... </div>
+                <div id="nav-trait-separateur3"> </div>
+            </div>
+            <div id="nav-block-contacts4">
+                <div id="nav-Nom-Prenom4"> Prénom Nom </div>
+                <div id="nav-ligne-block4"> Première ligne du dernier message envoyé ... </div>
+                <div id="nav-trait-separateur4"> </div>
+            </div>
+        </div>
+        
+        <img src="./public/img/profil.png" id="image"></img>
+        <div class="post-navigation-profile2">
+                <div id="Alpha7">Alpha 7</div>
+                <div id="FullStack">Full Stack</div>
+                
         </div>
 
-        <div id="nav-block-message"></div>
-        <div id="nav-message-recu"> Salut comment vas-tu ? </div>
+        <div id="nav-block-message">
+            <div id="nav-message-recu"> Salut comment vas-tu ? </div>
+        </div>
 
         <div id="nav-votre-message"> Votre Message : </div>
-        <div class="nav-element">
-                <input id="input-nav-bar" type="text" placeholder="Message..."></input>
-                <button class="hover-translate" onclick="search()">Envoyer</button>
-        </div>
+        <!-- <div class="nav-element">
+                <input id="input-nav-bar2" type="text" placeholder="Message..."></input>
+                <button id="pos" onclick="search()">Envoyer</button>
+        </div> -->
+        <input id="input-nav-bar2" type="text" placeholder="Message..."></input>
+        <button id="pos" onclick="search()">Envoyer</button>
 
 
         <div id="nav-Bas-de-Page">  </div>
+
+        <!-- <div id="nav-container-Chat"> </div>
+        <div id="nav-chat-mot">Chat </div>
+        <div id="nav-block-contacts1">
+            <div id="nav-Nom-Prenom1"> Prénom Nom </div>
+            <div id="nav-ligne-block1"> Première ligne du dernier message envoyé ... </div>
+            <div id="nav-trait-separateur1"> </div>
+        </div>
+        <div id="nav-block-contacts2">
+            <div id="nav-Nom-Prenom2"> Prénom Nom </div>
+            <div id="nav-ligne-block2"> Première ligne du dernier message envoyé ... </div>
+            <div id="nav-trait-separateur2"> </div>
+        </div>
+        <div id="nav-block-contacts3">
+            <div id="nav-Nom-Prenom3"> Prénom Nom </div>
+            <div id="nav-ligne-block3"> Première ligne du dernier message envoyé ... </div>
+            <div id="nav-trait-separateur3"> </div>
+        </div>
+        <div id="nav-block-contacts4">
+            <div id="nav-Nom-Prenom4"> Prénom Nom </div>
+            <div id="nav-ligne-block4"> Première ligne du dernier message envoyé ... </div>
+            <div id="nav-trait-separateur4"> </div>
+        </div> -->
 
     </div>
 
