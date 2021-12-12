8086ff00e7f9463142da9cd803873fc8bf348a95
lilyan <lilyan.bastien@gmail.com>
2021-12-12 15:30:52 +0100

    pageoffre

--- index.php
+++ index.php
@@ -26,35 +26,23 @@
 
     <div class="container">
         <div id="post-creation-container">
-            <h2>Nouveau post ?</h2>
-            <div class="post-creation-box">
-                <textarea id="post-creation-textarea" placeholder="Quelque chose à dire ..."></textarea>
-                <button>Publier</button>
-            </div>
+            Nouveau post
         </div>
         <div id="post-feed">
             <div class="post-container">
                 <div class="post-navigation">
-                    <div class="post-navigation-date">
-                        <p>01/12/2021 14h16</p>
-                    </div>
-                    <div class="post-navigation-profile">
+                    <div>01/12/2021 14h16</div>
+                    <div>
                         <img src="./public/img/profil.png"></img>
-                        <div class="post-navigation-profile-info">
-                            <h4>Alpha 7</h4>
-                            <h5>Full Stack</h5>
-                        </div>
+                        <h4>Alpha 7</h4>
+                        <h5>Full Stack</h5>
                     </div>
                 </div>
                 <div class="post-body">
                     <p>Fichier data csv !</p>
                 </div>
                 <div class="post-footer">
-                    <div>
-                        <span>189 pouces</span>
-                        <span>17 commentaires</span>
-                    </div>
-                    <span>share</span>
+
                 </div>
             </div>
         </div>
