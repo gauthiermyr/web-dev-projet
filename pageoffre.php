14a2b600ca9b624bbdea1a658a34ce6e0a976f0e
lilyan <lilyan.bastien@gmail.com>
2021-12-13 15:14:09 +0100

    footer et dynamic

--- pageoffre.php
+++ pageoffre.php
@@ -1,30 +1,6 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./public/css/template.css" rel="stylesheet">
-    <title>LeankDingue</title>
-</head>
-<body>
-    <div class="navigation">
-        <div id="nav-container">
-            <div class="nav-element">
-                <img src="./public/img/LeankDingue.png"></img>
-            </div>
-            <div class="nav-element">
-                <input id="input-nav-bar" type="text" placeholder="Recherche..."></input>
-                <button class="hover-translate" onclick="search()">Rechercher</button>
-            </div>
-            <div class="nav-element text-white">
-                <span onclick="alert('Invitation')" class="clickable hover-translate">Invitation</span>
-                <span onclick="alert('Chat')" class="clickable hover-translate">Chat</span>
-                <span onclick="alert('Profil')" class="clickable hover-translate">Profile</span>
-                <span onclick="alert('Deconnexion')" class="clickable hover-translate">Deconnexion</span>
-            </div>
-        </div>
-    </div>
+<?php
+        include('src/dynamic/header.php');
+?>
 
     <div class="container-offre">
         <div id="post-offre-container">
@@ -53,6 +29,10 @@
             <p id="contenu-offre1">offre pour stagière: blablabla</p>
         </div>
     </div>
+
+<?php
+        include('src/dynamic/footer.php');
+?>
 
     <script src="./public/js/main.js"></script>
 </body>
