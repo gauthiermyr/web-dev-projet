37a99cf84332b1a18d43584f53deed58d9be02e6
Pibastte <baptiste@feachy.com>
2021-12-29 13:40:32 +0100

    added include header and footer to login and register & added links to header

--- register.php
+++ register.php
@@ -1,28 +1,6 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./public/css/template.css" rel="stylesheet">
-    <title>LeankDingue</title>
-</head>
-<body>
-    <div class="navigation">
-        <div id="nav-container">
-            <div class="nav-element">
-                <img src="./public/img/LeankDingue.png"></img>
-            </div>
-            <div class="nav-element">
-                <input id="input-nav-bar" type="text" placeholder="Recherche..."></input>
-                <button class="hover-translate" onclick="search()">Rechercher</button>
-            </div>
-            <div class="nav-element text-white">
-                <span onclick="alert('login')" class="clickable hover-translate">Login</span>
-                <span onclick="alert('register')" class="clickable hover-translate">Register</span>
-            </div>
-        </div>
-    </div>
+<?php
+        include('src/dynamic/header.php');
+?>
     <div class="identification">
         <div class="identification-titre">
             <h2>Créer un compte</h2>
@@ -43,6 +21,12 @@
             <p><a href="login.php">Se connecter</a></p>
         </div>
     </div>
+    <script src="./public/js/main.js"></script>
+
+    <?php
+        include('src/dynamic/footer.php');
+    ?>
+
     <script src="./public/js/main.js"></script>
 </body>
 </html>
