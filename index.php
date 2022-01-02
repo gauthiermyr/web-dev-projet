94018166c8a86dbfb742cbff26109c932e6c47f1
Pibastte <baptiste@feachy.com>
2022-01-02 14:59:30 +0100

    dynamic loading of each post on the main page

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
             </div>
         </div>
         <div id="post-feed">
-            <div class="post-container">
+            <!-- <div class="post-container">
                 <div class="post-navigation">
                     <div class="post-navigation-date">
                         <p>01/12/2021 14h16</p>
@@ -33,63 +33,14 @@
                     </div>
                     <span>share</span>
                 </div>
-            </div>
-
-            <div class="post-container">
-                <div class="post-navigation">
-                    <div class="post-navigation-date">
-                        <p>01/12/2021 14h16</p>
-                    </div>
-                    <div class="post-navigation-profile">
-                        <img src="./public/img/profil.png"></img>
-                        <div class="post-navigation-profile-info">
-                            <h4>Alpha 7</h4>
-                            <h5>Full Stack</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="post-body">
-                    <p>Fichier data csv !</p>
-                </div>
-                <div class="post-footer">
-                    <div>
-                        <span>189 pouces</span>
-                        <span>17 commentaires</span>
-                    </div>
-                    <span>share</span>
-                </div>
-            </div>
-
-            <div class="post-container">
-                <div class="post-navigation">
-                    <div class="post-navigation-date">
-                        <p>01/12/2021 14h16</p>
-                    </div>
-                    <div class="post-navigation-profile">
-                        <img src="./public/img/profil.png"></img>
-                        <div class="post-navigation-profile-info">
-                            <h4>Alpha 7</h4>
-                            <h5>Full Stack</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="post-body">
-                    <p>Fichier data csv !</p>
-                </div>
-                <div class="post-footer">
-                    <div>
-                        <span>189 pouces</span>
-                        <span>17 commentaires</span>
-                    </div>
-                    <span>share</span>
-                </div>
-            </div>
+            </div> -->
         </div>
     </div>
 <?php
         include('src/dynamic/footer.php');
 ?>
 
-    <script src="./public/js/navigation.js"></script>
+    <script src="./public/js/navigation.js"></script>*
+    <script src="./public/js/main.js"></script>
 </body>
 </html>
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,71 @@
+const postsDic = [
+    {
+        date: "01/12/2021 14h16",
+        author: "Alpha 7",
+        bio: "Full Stack",
+        body: "Bonjour j'ai le plaisir de vous annoncer que je rejoins les équipes de LeankDingue !",
+        likes: 189,
+        nb_comments: 17
+    },
+    {
+        date: "01/12/2021 14h16",
+        author: "Alpha 7",
+        bio: "Full Stack",
+        body: "Bonjour j'ai le plaisir de vous annoncer que je rejoins les équipes de LeankDingue !",
+        likes: 189,
+        nb_comments: 17
+    },
+    {
+        date: "01/12/2021 14h16",
+        author: "Alpha 7",
+        bio: "Full Stack",
+        body: "Bonjour j'ai le plaisir de vous annoncer que je rejoins les équipes de LeankDingue !",
+        likes: 189,
+        nb_comments: 17
+    },
+]
 
+const postHTML = (postDic) => {
+    return (
+        "<div class='post-container'>" +
+            "<div class='post-navigation'>" +
+                "<div class='post-navigation-date'>" +
+                    "<p>" + postDic.date + "</p>" +
+                "</div>" +
+                "<div class='post-navigation-profile'>" +
+                    "<img src='./public/img/profil.png'></img>" +
+                    "<div class='post-navigation-profile-info'>" +
+                        "<h4>" + postDic.author + "</h4>" +
+                        "<h5>" + postDic.bio + "</h5>" +
+                    "</div>" +
+                "</div>" +
+            "</div>" +
+            "<div class='post-body'>" +
+                "<p>" + postDic.bio + "</p>" +
+            "</div>" +
+            "<div class='post-footer'>" +
+                "<div>" +
+                    "<span>" + postDic.likes + " pouces</span>" +
+                    "<span>" + postDic.nb_comments + " commentaires</span>" +
+                "</div>" +
+                "<span>share</span>" +
+            "</div>" +
+        "</div>"
+    )
+}
+
+const loadPosts = () => {
+    // fetch("./src/api/getPosts.php", {
+    //     method: 'GET'
+    // }).then((response) => {
+    //     // get list of posts and load them like below
+    // });
+
+    postsDic.forEach((postDic) => {
+        document.querySelector("#post-feed").insertAdjacentHTML("afterend", postHTML(postDic));
+    });
+}
 
 (() => {
-
+    loadPosts();
 })();
