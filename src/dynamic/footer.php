42cf3c48b8b71525944a9ed45b8c001eb8530476
lilyan <lilyan.bastien@gmail.com>
2022-01-03 22:11:01 +0100

    CG

--- src/dynamic/footer.php
+++ src/dynamic/footer.php
@@ -4,6 +4,6 @@
             <p>LeankDingue</p>
             <p><br>12 Av. <br>Léonard de Vinci <br>92400 Courbevoie</p>
         </div>
-            <p id="CG">CGU CGV Informations légales</p>
+            <p id="CG"><a href="https://github.com/gauthiermyr/web-dev-projet/doc/CGU">CGU</a> <a href="https://github.com/gauthiermyr/web-dev-projet/doc/informationsLegales">Informations légales</a></p>
     </div>
 </footer>
