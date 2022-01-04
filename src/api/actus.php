7f5e72182d80901bd6b487bd763b4c0f3ce224b8
Gauthier Mayer <legrangm@gmail.com>
2022-01-04 13:59:49 +0100

    order by date actus

--- src/api/actus.php
+++ src/api/actus.php
@@ -2,7 +2,7 @@
 
 require ('../db/database.php');
 
-$stmt = $db->query("SELECT *, Nom, Prenom, Photo  FROM actualites natural join utilisateurs");
+$stmt = $db->query("SELECT *, Nom, Prenom, Photo  FROM actualites natural join utilisateurs order by DatePublication asc");
 $res = $stmt->fetchAll(PDO::FETCH_OBJ);
 $actu = array();
 //foreach ($res as $actu){
