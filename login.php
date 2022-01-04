dbbf614babc806716155bbce0885311c40c9966a
Gauthier Mayer <legrangm@gmail.com>
2022-01-04 13:45:33 +0100

    Password field

--- login.php
+++ login.php
@@ -10,7 +10,7 @@
                 <input name="email" id="email-input" class="identification-write-input" type="text" placeholder="E-mail"></input>
             </div>
             <div class="identification-write clickable">
-                <input name="password" id="password-input" class="identification-write-input" type="text" placeholder="Mot de passe"></input>
+                <input name="password" id="password-input" class="identification-write-input" type="password" placeholder="Mot de passe"></input>
             </div>
             <div class="identification-valider">
                 <input type="submit" class="identification-valider-button clickable" onclick="">Se connecter</input>
